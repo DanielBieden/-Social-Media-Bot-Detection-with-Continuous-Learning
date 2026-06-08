270b14ea2e0c63196e10d16f63f7bddacc1ee2af
Max Zgonnik <max.zgonnik@uni-ulm.de>
2026-06-08 15:19:25 +0200

    fix: adjust PNN to actually freeze old weights

--- ClassificationHeads.py
+++ ClassificationHeads.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from triton.language import argmax
 
 
 class BaselineClassificationHead(nn.Module):
@@ -21,66 +22,44 @@ class BaselineClassificationHead(nn.Module):
 class ProgressiveNeuralNetworkClassifier(nn.Module):
     """
     A simple progressive neural network that expands in size to accommodate new classes.
-    On expansion the weights responsible for the old classes are frozen and don't change with gradient updates
+    On expansion a parallel classification head is added to accommodate new classes.
+    The old classification layers are removed from further optimization once a new layer is added
     """
     def __init__(self, in_features, output_dim, dropout_p = 0.1):
         super().__init__()
         self.in_features = in_features
         self.num_classes = output_dim
+        self.device = None # needed so that new layers are on the correct device
 
         self.dropout = nn.Dropout(p=dropout_p)
-        self.fc = nn.Linear(in_features=in_features, out_features=output_dim)
-        # add backpropagation hook to modify gradients during backpropagation
-        self.fc.weight.register_hook(self._weight_freeze_hook)
-        if self.fc.bias is not None:
-            self.fc.bias.register_hook(self._weight_freeze_hook)
-
-        # contains the indices of classes to have frozen
-        self.frozenLayers = []
+        # contains all output layers for classification
+        self.classificationLayers = nn.ModuleList([
+            nn.Linear(in_features=in_features, out_features=output_dim)
+        ])
 
     def forward(self, x):
-        x = self.dropout(x)
-        x = self.fc(x)
-        return x
+        # set the device once
+        if self.device is None: self.device = x.device
 
-    def _weight_freeze_hook(self, grad):
-        """
-        the function should be added as a hook during backpropagation. It ensures that gradients for frozen classes are not modified
-        by setting the gradients to 0.
-        :param grad: gradients for the gradient update
-        :return: the modified gradients
-        """
-        # zero out unwanted gradient updates in order to not change gradients
-        grad_clone = grad.clone()
-        grad_clone[self.frozenLayers] = 0
-        #print("gradient: " ,grad_clone)
-        return grad_clone
+        x = self.dropout(x)
+        # pass through all classification layers in parallel
+        outputs = list(map(lambda layer: layer(x), self.classificationLayers))
+        outputs = torch.cat(outputs, dim=-1) # concat outputs
+        return outputs
 
     def expand_classifier(self, num_to_add = 1):
         """
         dynamically expands the classifier by the number of classes specified.
-        Weights for the old classes are frozen and can't be trained anymore
+        Weights for the old classes are removed from the computation graph that is responsible for optimization
+         and can't be trained anymore
         :param num_to_add: the number of classes to add
+        :return: the new relevant parameters of the model to optimize
         """
-        self.frozenLayers = list(range(0,self.num_classes)) # update list to contain old classes
-        old_fc = self.fc
-        old_num_classes = self.num_classes
-        self.num_classes += num_to_add
-
-        # create new dense layer with added output size
-        new_fc = nn.Linear(self.in_features, self.num_classes)
-        new_fc = new_fc.to(device=old_fc.weight.device, dtype=old_fc.weight.dtype)
-
-        # copy the old weights and biases into the new layer (unpopulated weights are random)
-        # add backpropagation hook to layer weights, so that freezing works
-        with torch.no_grad():
-            new_fc.weight[:old_num_classes, :] = old_fc.weight
-            new_fc.weight.register_hook(self._weight_freeze_hook)
-            if old_fc.bias is not None:
-                new_fc.bias[:old_num_classes] = old_fc.bias
-                new_fc.bias.register_hook(self._weight_freeze_hook)
-
-        # replace layer
-        self.fc = new_fc
-
-
+        # disable optimization for old layers
+        for layer in self.classificationLayers:
+            for param in layer.parameters():
+                param.requires_grad = False
+        # add new layer to list
+        new_layer = nn.Linear(self.in_features, num_to_add).to(device=self.device)
+        self.classificationLayers.append(new_layer)
+        return new_layer.parameters()
