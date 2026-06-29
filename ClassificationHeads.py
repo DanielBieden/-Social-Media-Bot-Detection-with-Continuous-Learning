478b146c8ed137f7edece3b7ae4715217f6371e9
Max Zgonnik <max.zgonnik@uni-ulm.de>
2026-06-29 21:45:25 +0200

    feat: add SVM and Confidence classifiers

--- ClassificationHeads.py
+++ ClassificationHeads.py
@@ -4,15 +4,19 @@ from torch import nn
 
 class BaselineClassificationHead(nn.Module):
     """
-    The baseline classification head is essentially a normal Neural Network without any continuous learning aspects.
+    The baseline classification head is a normal neural network without any continuous learning aspects.
     It is used to determine the upper bound of our backbone.
     """
     def __init__(self, embedding_dim, output_dim, dropout_prob = 0.1):
         super().__init__()
         self.dropout = nn.Dropout(dropout_prob)
-        self.classifier = nn.Linear(in_features=embedding_dim, out_features=output_dim)
+        self.hidden = nn.Linear(embedding_dim, max(round(embedding_dim/2), output_dim))
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(in_features=max(round(embedding_dim/2), output_dim), out_features=output_dim)
     def forward(self, x):
         x = self.dropout(x)
+        x = self.hidden(x)
+        x = self.relu(x)
         x = self.classifier(x)
         return x
 
@@ -62,3 +66,166 @@ class ProgressiveNeuralNetworkClassifier(nn.Module):
         new_layer = nn.Linear(self.in_features, num_to_add).to(device=self.device)
         self.classificationLayers.append(new_layer)
         return new_layer.parameters()
+
+
+
+class SVM(nn.Module):
+    """
+    a single support vector machine (SVM) implemented as a single linear layer.
+    Use hinge loss and L2 regularization for it to behave like a SVM.
+    """
+    def __init__(self, in_features):
+        """
+        :param in_features: dimension of the input features
+        """
+        super().__init__()
+        self.classifier = nn.Linear(in_features=in_features, out_features=1)
+    def forward(self, x):
+        """
+        :param x: input tensor
+        :return: tensor of logits, tensor of predictions
+        """
+        x = self.classifier(x)
+        return x, torch.sign(x)
+
+    def get_weights(self):
+        """
+        returns the weights of this model.
+        :return: weights of this model
+        """
+        return self.classifier.weight
+
+class OneAgainstRestSVM(nn.Module):
+    """
+
+    """
+    def __init__(self, in_features, output_dim):
+        super().__init__()
+        self.svms = nn.ModuleList([SVM(in_features=in_features) for _ in list(range(output_dim))])
+        self.in_features_dim = in_features
+        self.size = output_dim
+        self.device = None  # needed so that new layers are on the correct device
+
+    def forward(self, x):
+        # set the device once
+        if self.device is None: self.device = x.device
+        # handle forward pass
+        outputs = list(map(lambda svm: svm(x)[0], self.svms))
+        outputs = torch.cat(outputs, dim=-1) # concat outputs
+        return outputs
+
+    def get_weights(self):
+        """
+        returns the weights of this model.
+        :return: weights of this model
+        """
+        return torch.stack([ svm.classifier.weight for svm in self.svms ])
+    def expand_classifier(self, num_to_add = 1):
+        """
+        dynamically expands the classifier by the number of classes specified.
+        :param num_to_add: the number of classes to add
+        :return: the new relevant parameters of the model to optimize
+        """
+
+        for i in range(num_to_add):
+            new_svm = SVM(in_features=self.in_features_dim).to(device=self.device)
+            self.svms.append(new_svm)
+            self.size += 1
+        return self.parameters()
+
+
+class ReplayBuffer:
+    """
+    Replay Buffer that manages samples per class.
+    If added samples would exceed the buffer limit, then random old samples are replaced instead
+    """
+
+    def __init__(self, buffer_size: int, seed: int):
+        """
+        :param buffer_size: the maximum amount of samples to store per class/label
+        :param seed: seed, to keep sampling reproducible
+        """
+        self.buffer_size = buffer_size
+
+        # PyTorch random generator for reproducibility
+        self.rng = torch.Generator()
+        self.rng.manual_seed(seed)
+
+        # dictionary to act as a buffer for each class/label
+        self.buffer = {}
+
+    def add_samples(self, labels, samples):
+        """
+        adds samples to the replay buffer.
+        If the buffer is full, the provided samples are used to replace existing samples.
+        :param labels: list of labels for the samples
+        :param samples: list of samples to store
+        """
+
+        # check if input dimensions match
+        assert (len(labels) == len(samples)), "Labels and samples must have the same length."
+        labels = labels.detach()
+        samples = samples.detach()
+
+        for label, sample in zip(labels, samples):
+            label = label.item()
+            # Initialize list if class hasn't been seen yet
+            if label not in self.buffer:
+                self.buffer[label] = []
+
+            # add sample to buffer, if buffer size is reached use it to replace an old sample
+            class_buffer = self.buffer[label]
+            if len(class_buffer) < self.buffer_size: # just append
+                class_buffer.append(sample)
+            else: # randomly replace one
+                index = torch.randint(0, self.buffer_size, (1,), generator=self.rng).item()
+                class_buffer[index] = sample
+
+    def get_samples(self, samples_per_class: int):
+        """
+        Provides a random sample from each stored sample. Samples are without duplicates,
+        meaning if fewer samples are available in the buffer than requested, fewer samples will be returned
+        :param samples_per_class: how many samples per class to return
+        :return: a tensor of labels, and a tensor of samples
+        """
+
+        out_labels = []
+        out_samples = []
+
+        # loop through each stored class buffer and add to the sample
+        for key, class_buffer in self.buffer.items():
+            current_count = len(class_buffer)
+            # skip, if no samples are available
+            if current_count == 0: continue
+            # limit samples to available amount
+            num_to_draw = min(samples_per_class, current_count)
+
+            # Generate random indices without replacement
+            indices = torch.randperm(current_count, generator=self.rng)[:num_to_draw]
+
+            # go through random indexes and add samples to list
+            for idx in indices:
+                out_labels.append(key)
+                out_samples.append(class_buffer[idx.item()])
+
+        if out_samples == []: return torch.tensor([]), torch.tensor([])
+        sampled_labels = torch.tensor(out_labels)
+        sampled_data = torch.stack(out_samples)
+
+        return sampled_labels, sampled_data
+
+class ConfidenceClassifier(nn.Module):
+    def __init__(self, embedding_dim, output_dim, confidence_threshold = 0.7, dropout_prob = 0.1):
+        super().__init__()
+        self.confidence_threshold = confidence_threshold
+        self.dropout = nn.Dropout(dropout_prob)
+        self.hidden = nn.Linear(embedding_dim, max(round(embedding_dim/2), output_dim))
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(in_features=max(round(embedding_dim/2), output_dim), out_features=output_dim)
+        self.softmax = nn.Softmax(dim=-1)
+    def forward(self, x):
+        x = self.dropout(x)
+        x = self.hidden(x)
+        x = self.relu(x)
+        x = self.classifier(x)
+        return x, self.softmax(x), self.softmax(x) > self.confidence_threshold # logits and probabilities
--- pipeline_utils.py
+++ pipeline_utils.py
@@ -133,3 +133,125 @@ def test_classifier(classifier, input_samples, ground_truth_labels, device = 'cu
         predictions.extend(prediction.detach().cpu().numpy())
         ground_truths.extend(targets.detach().cpu().numpy())
     return ground_truths, predictions
+
+
+def multi_svm_training_loop(classifier, replay_buffer, input_samples, ground_truth_labels, epochs = 15, batch_size = 128, device = 'cuda' if torch.cuda.is_available() else 'cpu'):
+    # general setup
+    C = 1.0
+    learning_rate = 0.1
+    optimizer = torch.optim.SGD(classifier.parameters(), lr=learning_rate)
+
+    classifier.train()
+    # create dataset from ground_truths and embedding features for batch processing
+    input_tensors = torch.tensor(np.array(input_samples))
+    ground_truth_tensors = torch.tensor(ground_truth_labels)
+    dataset = TensorDataset(input_tensors, ground_truth_tensors)
+    train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    for epoch in range(epochs):
+        classifier.train()
+        running_loss = 0.0
+
+        predictions = []
+        ground_truths = []
+
+        for batch_idx, (inputs, targets) in enumerate(train_dataloader):
+            dataset_inputs = inputs.float().to(device)
+            dataset_targets = targets.to(device)
+
+            # sample buffer and concatenate it to dataset sample
+            buffer_targets, buffer_inputs = replay_buffer.get_samples(round(batch_size/8))
+
+            inputs = torch.concat((dataset_inputs, buffer_inputs.to(device)))
+            targets = torch.concat((dataset_targets, buffer_targets.to(device))).int()
+
+            # encode targets in a binary fashion
+            remapped_targets = torch.full((len(targets), classifier.size), -1).to(device)
+            remapped_targets[torch.arange(len(targets)), targets] = 1
+
+            # pass data through classifier and calculate loss
+            optimizer.zero_grad()
+            outputs = classifier(inputs)
+
+
+            # calculate loss
+            regularization_loss = 0.5 * torch.sum(classifier.get_weights() ** 2)
+            hinge_loss = torch.mean(torch.clamp(1 - remapped_targets * outputs, min=0))
+            loss = regularization_loss + C * hinge_loss
+            loss.backward()
+            optimizer.step()
+
+            # prediction
+            prediction = torch.argmax(outputs, dim=1)
+            # add samples to the buffer
+            replay_buffer.add_samples(targets[:16].detach(), inputs[:16].detach())
+
+            # statistics
+            predictions.extend(prediction[:batch_size].detach().cpu().numpy())
+            ground_truths.extend(targets[:batch_size].detach().cpu().numpy())
+            running_loss += loss.item() * inputs.size(0)
+
+        # Calculate average loss across the entire epoch
+        epoch_loss = running_loss / len(input_samples)
+        print(f"Epoch [{epoch + 1}/{epochs}] - Loss: {epoch_loss:.4f}")
+    return ground_truths, predictions
+
+def confidence_classifier_training_loop(classifier, criterion, optimizer ,input_samples, ground_truth_labels, epochs = 15, bot_label = 0, batch_size = 128, device = 'cuda' if torch.cuda.is_available() else 'cpu'):
+    classifier.train()
+    # create dataset from ground_truths and embedding features for batch processing
+    input_tensors = torch.tensor(np.array(input_samples))
+    ground_truth_tensors = torch.tensor(ground_truth_labels)
+    dataset = TensorDataset(input_tensors, ground_truth_tensors)
+    train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    inconfident_samples_buffer = []
+
+    for epoch in range(epochs):
+        classifier.train()
+        running_loss = 0.0
+
+        predictions = []
+        ground_truths = []
+
+        for batch_idx, (inputs, targets) in enumerate(train_dataloader):
+            inputs = inputs.float().to(device)
+            targets = targets.to(device)
+
+            # sample from buffer
+            sample_size = min(len(inconfident_samples_buffer), 16)
+            buffer_sample = inconfident_samples_buffer[:sample_size]
+            inconfident_samples_buffer = inconfident_samples_buffer[sample_size:]
+
+            if sample_size != 0:
+                buffer_sample = torch.stack(buffer_sample).to(device)
+                buffer_targets = torch.full((sample_size, ), bot_label).to(device)
+
+                # concat
+                inputs = torch.concat((inputs, buffer_sample)).to(device)
+                targets = torch.concat((targets, buffer_targets)).to(device)
+
+            # pass data through classifier and calculate loss
+            outputs, prob, is_confident = classifier(inputs)
+            loss = criterion(outputs, targets)
+
+            # optimize classifier
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # prediction
+            prediction = torch.argmax(outputs, dim=1)
+
+            # add to buffer if confidence is below the threshold
+            to_add = inputs[torch.sum(is_confident,1) == 0].detach()
+            inconfident_samples_buffer.extend(to_add)
+
+            # statistics
+            predictions.extend(prediction.detach().cpu().numpy())
+            ground_truths.extend(targets.detach().cpu().numpy())
+            running_loss += loss.item() * inputs.size(0)
+
+        # Calculate average loss across the entire epoch
+        epoch_loss = running_loss / len(input_samples)
+        print(f"Epoch [{epoch + 1}/{epochs}] - Loss: {epoch_loss:.4f}")
+    return ground_truths, predictions
